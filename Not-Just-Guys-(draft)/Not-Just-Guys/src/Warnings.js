f78078ca70173b43daace26b80d32f70a18213e7
genEquality <46731486+shinnyc@users.noreply.github.com>
2019-03-08 11:00:12 -0500

    Update Warnings
    Added additional words and phrases.

--- Not-Just-Guys-(draft)/Not-Just-Guys/src/Warnings.js
+++ Not-Just-Guys-(draft)/Not-Just-Guys/src/Warnings.js
@@ -13,13 +13,42 @@ var WARNINGS = {
     {
       "keyword": "Congressman",
       "source": "http://www.genequality.org",
-      "message": "Are you actually referring to a male representative? If not, how about using 'Congressperson' instead?"
+      "message": "Are you referring to a specific male representative? If not, how about using 'legislator' or 'Representative' instead?"
     },
     {
       "keyword": "fireman",
       "source": "http://www.genequality.org",
       "message": "How about saying 'firefighter' instead?"
     },
+    {
+      "keyword": "mankind",
+      "source": "http://www.genequality.org",
+      "message": "How about saying 'humankind' instead?"
+    },
+    {
+      "keyword": "ladies and gentlemen",
+      "source": "http://www.genequality.org",
+      "message": "How about saying 'esteemed colleagues' or 'everyone' instead?"
+    }{
+      "keyword": "policeman",
+      "source": "http://www.genequality.org",
+      "message": "How about saying 'police officer' instead?"
+    },
+    {
+      "keyword": "policewoman",
+      "source": "http://www.genequality.org",
+      "message": "How about saying 'police officer' instead?"
+    }
+    {
+      "keyword": "chairman",
+      "source": "http://www.genequality.org",
+      "message": "How about saying 'Chair' or 'chairperson' instead?"
+    },
+    {
+      "keyword": "those guys",
+      "source": "http://www.genequality.org",
+      "message": "Is everyone you're referring to a man? No? How about saying 'those people' instead?"
+    }
     {
       "keyword": "postman",
       "source": "http://www.genequality.org",
